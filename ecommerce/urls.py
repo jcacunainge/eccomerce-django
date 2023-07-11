62e4176d856f804cc43ffb366592c34ebcefbc0f
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2023-07-10 19:53:57 -0500

    Actualización

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('usuario/', include('usuarios.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROTT)
